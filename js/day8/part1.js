d4ff58a389b7825f4b775744809106d00ff29c1a
Alex Consunji <Alexander.Consunji@slalom.com>
2021-12-15 15:47:05 -0500

    Working pt1

--- js/day8/part1.js
+++ js/day8/part1.js
@@ -1,9 +1,22 @@
-const fs = require('fs')
+const fs = require('fs');
+const { connected } = require('process');
  
-var contents = fs.readFileSync('./test.txt', 'utf8');
+var contents = fs.readFileSync('./input.txt', 'utf8');
 // var contents = fs.readFileSync('./input.txt', 'utf8');
 // write function to grab all attendees
 // example parse
 // Format
-const input = contents.split(',').map((x)=> parseInt(x));
+const input = contents.split(/\r?\n/).map((x)=> x.split(' | '));
+let oneFourSevenEight = []
 
+for(line of input) {
+  const values = line[1].split(' ')
+  values.forEach(code => {
+    // 1 , 4, 7, 8
+    if(code.length == 2 || code.length == 4 || code.length == 3 || code.length == 7) {
+      oneFourSevenEight.push(code)
+    }
+  })
+}
+
+console.log(oneFourSevenEight.length)
