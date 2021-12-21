de1f602cdbb4866e32e2c57b386aae98fdb3b90e
Alex Consunji <Alexander.Consunji@slalom.com>
2021-12-21 14:13:10 -0500

    day4 and day5 start

--- src/day5.rs
+++ src/day5.rs
@@ -3,60 +3,56 @@ use std::io::BufReader;
 use std::io::BufRead;
 
 fn main() {
-    // --snip--
-    // let args: Vec<String> = env::args().collect();
+  // --snip--
+  // let args: Vec<String> = env::args().collect();
 
-    // let query = &args[1];
-    // let filename = &args[2];
+  // let query = &args[1];
+  // let filename = &args[2];
 
-    // println!("Searching for {}", query);
-    // println!("In file {}", filename);
+  // println!("Searching for {}", query);
+  // println!("In file {}", filename);
 
-    let contents = load_from_file("./day1input1.txt");
-    // let part1Answer =  part1(contents);
-    // println!("Part 1 {}", part1Answer);
-    let part2Answer =  part2(contents);
-    println!("Part 2 {}", part2Answer);
+  let contents = load_from_file("./inputs/day2input.txt");
+  // let part1Answer =  part1(contents);
+  // println!("Part 1 {}", part1Answer);
+  let part2Answer =  part2(contents);
+  println!("Part 2 {}", part2Answer);
+}
+
+#[derive(Debug)]
+struct Command {
 }
 
 fn part1(input: Vec<i32>) -> i32 {
-  let mut increases = 0;
-  let mut lastVal = 0;
-  for val in input.iter() {
-    if &lastVal < val {
-      increases += 1;
-    }
-    lastVal = *val;
-  }
-  return increases - 1;
+  return -1
 }
 
 fn part2(input: Vec<i32>) -> i32 {
-  let mut increases = 0;
-  let mut lastSum = 0;
-  let mut currentSum = 0;
-  
-  for (index, val) in input.iter().enumerate() {
-    if index > 1 && index < 1999 {
-      println!("Part 2 previous values {} {} {}", input[index-1], input[index-2], val);
-      currentSum = val + input[index-1] + input[index-2];
-      lastSum = val + input[index-1] + input[index+1];
-      println!("Part 2 compare {} {} ", currentSum, lastSum);
-      if lastSum > currentSum {
-        increases += 1;
-      }
-    }
-  }
-  return increases;
+  return -1
 }
 
-fn load_from_file(file_path: &str) -> Vec<i32> {
-    let file = File::open(file_path).expect("file wasn't found.");
-    let reader = BufReader::new(file);
+fn load_from_file(file_path: &str) -> (Vec<i32>) {//, vec<vec<i32>>) {
+  let file = File::open(file_path).expect("file wasn't found.");
+  let reader = BufReader::new(file);
 
-    let numbers: Vec<i32> = reader
-        .lines()
-        .map(|line| line.unwrap().parse::<i32>().unwrap())
-        .collect();
-    return numbers;
+  let lines: Vec<String> = reader
+      .lines()
+      .map(|line| line.expect("Could not parse line"))
+      .collect();
+
+  let numbers: Vec<i32> = lines.pop().map(|line| line.split(',').unwrap().parse::<i32>()).collect()
+
+  // let mut commands = Vec::new();
+  // for line in lines {
+  //     let mut split = line.split_whitespace();
+  //     let dir = split.next().unwrap();
+  //     // println!("dir {:?}", dir);
+  //     let num = split.next().unwrap().parse::<i32>().unwrap();
+  //     // println!("num {:?}", num);
+  //     let new_command = {Command {direction: dir.to_string(), number: num}};
+  //     commands.push(new_command);
+  //   // let com = line.split_whitespace().next().next();
+  // }
+  println!("numbers {:?}", numbers);
+  return numbers;
 }
--- src/main.rs
+++ src/main.rs
@@ -3,220 +3,60 @@ use std::io::BufReader;
 use std::io::BufRead;
 
 fn main() {
+  // --snip--
+  // let args: Vec<String> = env::args().collect();
 
-    let contents = load_from_file("./inputs/day3input.txt");
-    // let part_1_answer =  part1(contents);
-    // println!("Part 1 {}", part_1_answer);
-    let part_2_answer =  part2(contents);
-    println!("Part 2 {}", part_2_answer);
+  // let query = &args[1];
+  // let filename = &args[2];
+
+  // println!("Searching for {}", query);
+  // println!("In file {}", filename);
+
+  let contents = load_from_file("./inputs/test.txt");
+  // let part1Answer =  part1(contents);
+  // println!("Part 1 {}", part1Answer);
+  let part2Answer =  part2(contents);
+  println!("Part 2 {}", part2Answer);
 }
 
-fn part1(input: Vec<Vec<char>>) -> i32 {
-  let mut gamma: Vec<char> = vec![];
-  let mut epsilon: Vec<char> = vec![];
-  // let all_bin = input.len();
-  for y in 0..input[0].len() {
-    let mut zero_count = 0;
-    let mut one_count = 0;
-
-    for x in 0..input.len() {
-      let current = input[x][y];
-      if current == '0' {
-        zero_count+=1;
-      }
-      else if current == '1' {
-        one_count+=1;
-      }
-    }
-    if zero_count > one_count {
-      gamma.push('0');
-      epsilon.push('1');
-    } else {
-      gamma.push('1');
-      epsilon.push('0');
-    }
-
-  }
-  let g: String = gamma.into_iter().collect();
-  let e: String = epsilon.into_iter().collect();
-  println!("gamma {}", g);
-  println!("epsil {}", e);
-  let gamma_val = isize::from_str_radix(&g, 2).unwrap();
-  let epsilon_val = isize::from_str_radix(&e, 2).unwrap();
-  println!("gamma val {}", gamma_val);
-  println!("epsil val {}", epsilon_val);
-  return (gamma_val * epsilon_val).try_into().unwrap()
-  // return -1
+#[derive(Debug)]
+struct Command {
 }
 
-
-
-fn part2(input: Vec<Vec<char>>) -> i32 {
-  let mut oxygen: Vec<Vec<char>> = input.clone();
-  let mut carbon: Vec<Vec<char>> = input.clone();
-
-  // let all_bin = input.len();
-  // keep = input[y].clone()
-  let mut y = 0;
-  while oxygen.len() != 1 {
-    let mut ox_index: Vec<Vec<char>> = vec![];
-    let mut ca_index: Vec<Vec<char>> = vec![];
-    //
-    // for mut y in 0..oxygen[0].len() {
-      
-      let mut zero_count = 0;
-      let mut one_count = 0;
-
-      for x in 0..oxygen.len() {
-        let current = oxygen[x][y].clone();
-        let current_row = oxygen[x].clone();
-        println!("current val {:?}", current);
-        println!("current row val {:?}", current_row);
-        if current == '0' {
-          zero_count+=1;
-          ca_index.push(current_row);
-        }
-        else if current == '1' {
-          one_count+=1;
-          ox_index.push(current_row);
-        }
-      }
-      // println!("ox_index val {:?}", ox_index);
-      // println!("ca_index val {:?}", ca_index);
-
-      // println!("input at 0,5 {:?}", input[0]);
-      // let mut new_ox: Vec<Vec<char>> = vec![vec![]];
-      if zero_count > one_count {
-        // for index in ox_index.iter() {
-        //   new_ox.push(index.to_vec())
-        // }
-        oxygen = ca_index.clone();
-
-      } 
-      // if more ones than zeros
-      else {
-        // for index in ca_index.iter() {
-        //   new_ox.push(index.to_vec())
-        // }
-        oxygen = ox_index.clone();
-      }
-      // println!("ca_index val {:?}", new_ox);
-      // y = 0;
-      y+=1;
-      // println!("after oxygen val {:?}", oxygen);
-      // println!("ca_index val {:?}", oxygen);
-    // }
-  }
-
-  y = 0;
-  while carbon.len() != 1 {
-    let mut ox_index: Vec<Vec<char>> = vec![];
-    let mut ca_index: Vec<Vec<char>> = vec![];
-    //
-    // for mut y in 0..oxygen[0].len() {
-      
-      let mut zero_count = 0;
-      let mut one_count = 0;
-
-      for x in 0..carbon.len() {
-        let current = carbon[x][y].clone();
-        let current_row = carbon[x].clone();
-        println!("current val {:?}", current);
-        println!("current row val {:?}", current_row);
-        if current == '0' {
-          zero_count+=1;
-          ca_index.push(current_row);
-        }
-        else if current == '1' {
-          one_count+=1;
-          ox_index.push(current_row);
-        }
-      }
-      // println!("ox_index val {:?}", ox_index);
-      // println!("ca_index val {:?}", ca_index);
-
-      // println!("input at 0,5 {:?}", input[0]);
-      // let mut new_ox: Vec<Vec<char>> = vec![vec![]];
-      if zero_count <= one_count {
-        // for index in ox_index.iter() {
-        //   new_ox.push(index.to_vec())
-        // }
-        carbon = ca_index.clone();
-
-      } 
-      // if more ones than zeros
-      else {
-        // for index in ca_index.iter() {
-        //   new_ox.push(index.to_vec())
-        // }
-        carbon = ox_index.clone();
-      }
-      // println!("ca_index val {:?}", new_ox);
-      // y = 0;
-      y+=1;
-      // println!("after oxygen val {:?}", oxygen);
-      // println!("ca_index val {:?}", oxygen);
-    // }
-  }
-
-  // while carbon.len() != 1 {
-  //   let mut oxIndex: Vec<Vec<char>> = vec![];
-  //   let mut caIndex: Vec<Vec<char>> = vec![];
-    
-  //   //
-  //   for y in 0..input[0].len() {
-      
-  //     let mut zero_count = 0;
-  //     let mut one_count = 0;
-
-  //     for x in 0..input.len() {
-  //       let current = input[x][y];
-  //       if current == '0' {
-  //         zero_count+=1;
-  //         oxIndex.push(current)
-  //       }
-  //       else if current == '1' {
-  //         one_count+=1;
-  //         caIndex.push(current)
-  //       }
-  //     }
-
-  //     if one_count < zero_count {
-  //       carbon = oxIndex.clone()
-  //     } else {
-  //       carbon = caIndex.clone()
-  //     }
-  //   }
-  // }
-  let o_val = &oxygen[0];
-  let c_val = &carbon[0];
-  println!("final oxygen val {:?}", o_val);
-  println!("final carbon val {:?}", c_val);
-  let o: String = o_val.into_iter().collect();
-  let c: String = c_val.into_iter().collect();
-  println!("oxygen bit {}", o);
-  println!("carbon bit {}", c);
-  let oxygen_val = isize::from_str_radix(&o, 2).unwrap();
-  let carbon_val = isize::from_str_radix(&c, 2).unwrap();
-  // println!("oxygen val {}", oxygen_val);
-  // println!("carbon val {}", carbon_val);
-  return (oxygen_val * carbon_val).try_into().unwrap()
-  // return -1
+fn part1(input: Vec<i32>) -> i32 {
+  return -1
 }
 
-fn load_from_file(file_path: &str) -> Vec<Vec<char>> {
+fn part2(input: Vec<i32>) -> i32 {
+  return -1
+}
+
+fn load_from_file(file_path: &str) -> Vec<i32> {//, vec<vec<i32>>) {
   let file = File::open(file_path).expect("file wasn't found.");
   let reader = BufReader::new(file);
 
-  let lines: Vec<String> = reader
+  let mut lines: Vec<String> = reader
       .lines()
       .map(|line| line.expect("Could not parse line"))
       .collect();
+  let mut firstLine = &lines[0];
+  println!("firstLine {:?}", firstLine);
+  lines.remove(0);
+  println!("lines {:?}", lines);
+  let numbers: Vec<i32> = Vec::new(); //= firstLine.pop().map(|line| line.split(',').unwrap().parse::<i32>()).collect();
 
   let mut commands = Vec::new();
-    for line in lines {
-      let char_vec: Vec<char> = line.chars().collect();
-      commands.push(char_vec)
-    }
-    return commands;
+  for line in lines {
+      let mut split = line.split_whitespace();
+      let dir = split.next().unwrap();
+      // println!("dir {:?}", dir);
+      let num = split.next().unwrap().parse::<i32>().unwrap();
+      // println!("num {:?}", num);
+      let new_command = {Command {direction: dir.to_string(), number: num}};
+      commands.push(new_command);
+    // let com = line.split_whitespace().next().next();
+  }
+  println!("numbers {:?}", numbers);
+  
+  return numbers;
 }
