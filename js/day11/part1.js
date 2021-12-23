ea77344b30a2ee16d1f2a01101ac3b5e17050239
Alex Consunji <Alexander.Consunji@slalom.com>
2021-12-23 17:22:01 -0500

    Switch laptop

--- js/day11/part1.js
+++ js/day11/part1.js
@@ -11,53 +11,77 @@ const input = contents.split(/\r?\n/).map((x)=> x.split('').map(val => parseInt(
 // find total number of flashes
 let flashed = new Set()
 const incrementAround = (y,x) => {
-    // console.log("searching current", coordinates);
-    // use an object and loop through all of these
-    let current = JSON.stringify({y,x})
 
     const positions = {
     // 'current' : { flashCount: input[y][x], position: {y,x}},
-    // corners are wrong down up left, up right, down left
 
-    'left' : { flashCount: input?.[y-1]?.[x] ? input?.[y-1][x] : 99, position: {y:(y-1), x}},
+    'left' : { flashCount: input?.[y]?.[x-1] ? input?.[y][x-1] : 99, position: {y:(y), x:(x-1)}},
 
     'upLeft' : { flashCount: input?.[y-1]?.[x-1] ? input?.[y-1][x-1] : 99, position: {y:(y-1),x:(x-1)}},
 
-    'up' : { flashCount: input?.[y]?.[x+1] ? input?.[y]?.[x+1] : 99, position: {y, x:(x+1)}},
+    'up' : { flashCount: input?.[y-1]?.[x] ? input?.[y-1]?.[x] : 99, position: {y:(y-1), x:(x)}},
 
     'upRight' : { flashCount: input?.[y-1]?.[x+1] ? input?.[y-1]?.[x+1] : 99, position: {y:(y-1),x:(x+1)}},
 
-    'right' : { flashCount: input?.[y+1]?.[x] ? input?.[y+1]?.[x] : 99, position: {y:(y+1), x}},
+    'right' : { flashCount: input?.[y]?.[x+1] ? input?.[y]?.[x+1] : 99, position: {y:(y), x:(x+1)}},
 
     'downLeft' : { flashCount: input?.[y+1]?.[x-1] ? input?.[y+1][x-1] : 99, position: {y: (y+1), x:(x-1)}},
 
-    'down' : { flashCount: input?.[y]?.[x-1] ? input?.[y]?.[x-1] : 99, position: {y, x: (x-1)}},
+    'down' : { flashCount: input?.[y+1]?.[x] ? input?.[y+1]?.[x] : 99, position: {y:(y+1), x: (x)}},
 
     'downRight' : { flashCount: input?.[y+1]?.[x+1] ? input?.[y+1][x+1] : 99, position: {y:(y+1), x:(x+1)}}
     }
 
     let currentSum = 0
-    if(!flashed.has(JSON.stringify(current)))
-    Object.values(positions).forEach(val => {
-      let posX = val.position.x
-      let posY = val.position.y
-      let current = {y: posY, x: posX}
-      // check if the position has flashed and if the value exists
-      if(val.flashCount != 99 && !flashed.has(JSON.stringify(current))) {
-        // console.log("x,y", x,y)
-        // console.log("current", input)
-        // console.log('looking at x,y', posX, posY)
-        if(input[posY][posX] >= 10) {
-          // increment flashes
-          flashed.add(JSON.stringify(current))
-          currentSum++;
-          input[posY][posX] = 0
-          currentSum += incrementAround(posY,posX)
-        } else {
-          input[posY][posX]++
-        }
+    // check if the value has flashed
+    // if(!flashed.has(current)) {
+      if(input[y][x] >= 10) {
+          //increment all non 10s and 
+          Object.values(positions).forEach(val => {
+            if(val.flashCount != 99) {
+              let posX = val.position.x
+              let posY = val.position.y
+              let temp = {y: posY, x: posX}
+              console.log(`Flash at with pos x:${posX} y:${posY}`)
+
+              let evalPos = JSON.stringify(temp)
+              if(input[posY][posX] != 10 && input[posY][posX] != 0 && !flashed.has(evalPos)) input[posY][posX]++
+              else if(input[posY][posX] == 10) {
+                input[posY][posY] = 0
+                flashed.add(evalPos)
+                incrementAround(posY,posX);
+              }
+              else if(input[posY][posX] == 0 && !flashed.has(evalPos)) {
+                input[posY][posX]++
+              }
+            }
+          })
+
+          // Object.keys(positions).forEach(key => {
+          //   let val = positions[key]
+          //   let posX = val.position.x
+          //   let posY = val.position.y
+          //   let evalPos = {y: posY, x: posX}
+          //   // check if the position has flashed and if the value exists
+          //   if(val.flashCount != 99 && !flashed.has(JSON.stringify(evalPos))) {
+          //     if(val.flashCount >= 10) {
+          //       // increment flashes
+          //       console.log(`Current eval x:${x} y:${y}`)
+          //       console.log(`Flash at ${key} with pos x:${posX} y:${posY}`)
+          //       flashed.add(JSON.stringify(evalPos))
+          //       currentSum++;
+          //       input[posY][posX] = 0
+          //       currentSum += incrementAround(posY,posX)
+          //     } 
+          //     else {
+          //       input[posY][posX]++
+          //     }
+          //   }
+          // })
+      // } else if(!flashed.has(JSON.stringify(current))) {
+        // input[y][x]++
       }
-    })
+    // }
     // console.log(x,y, input)
     return currentSum
 
@@ -74,15 +98,19 @@ for(let n = 0; n < 2; n++) {
     }
   }
   // console.log(incrementAround(2,2))
+  console.log("step ", (n+1))
   console.log(input)
 
   for(let y=0; y < input[0].length; y++) {
     for(let x=0; x < input.length; x++) {
-      totalFlash += incrementAround(y,x)
+      if(input[y][x] == 10) {incrementAround(y,x);}
+      totalFlash += flashed.size
     }
   }
   flashed = new Set()
+  console.log(`step ${(n+1)} after flash`)
   console.log(input)
+
 }
 console.log("after flash", flashed)
 
