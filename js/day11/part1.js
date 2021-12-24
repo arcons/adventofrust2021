ba22e505e088ac85b1ee09db25f246a342f5f6a8
Alex Consunji <arcon1993@gmail.com>
2021-12-23 20:58:52 -0500

    Base example

--- js/day11/part1.js
+++ js/day11/part1.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const { json } = require('node:stream/consumers');
  
 var contents = fs.readFileSync('./firstTest.txt', 'utf8');
 // var contents = fs.readFileSync('./input.txt', 'utf8');
@@ -29,60 +28,48 @@ const incrementAround = (y,x) => {
 
     'down' : { flashCount: input?.[y+1]?.[x] ? input?.[y+1]?.[x] : 99, position: {y:(y+1), x: (x)}},
 
-    'downRight' : { flashCount: input?.[y+1]?.[x+1] ? input?.[y+1][x+1] : 99, position: {y:(y+1), x:(x+1)}}
+    'downRight' : { flashCount: input?.[y+1]?.[x+1] ? input?.[y+1][x+1] : 99, position: {y:(y+1), x:(x+1)}},
+
     }
 
     let currentSum = 0
     // check if the value has flashed
     // if(!flashed.has(current)) {
-      if(input[y][x] >= 10) {
+      // if(input[y][x] >= 10) {
           //increment all non 10s and 
-          Object.values(positions).forEach(val => {
+          Object.keys(positions).forEach(key => {
+            console.log(key)
+            val = positions[key]
             if(val.flashCount != 99) {
               let posX = val.position.x
               let posY = val.position.y
               let temp = {y: posY, x: posX}
-              console.log(`Flash at with pos x:${posX} y:${posY}`)
 
               let evalPos = JSON.stringify(temp)
-              if(input[posY][posX] != 10 && input[posY][posX] != 0 && !flashed.has(evalPos)) input[posY][posX]++
-              else if(input[posY][posX] == 10) {
-                input[posY][posY] = 0
+              // if(input[posY][posX] != 10 && input[posY][posX] != 0 && !flashed.has(evalPos)) {
+                // input[posY][posX]++
+              // }
+              if(input[posY][posX] >= 10 && !flashed.has(evalPos)) {
+                input[posY][posX] = 0
+                if(posX == 1 & posY == 1) {
+                  console.log('figure out double increment')
+                }
+                console.log(`Flash at with pos x:${posX} y:${posY}`)
                 flashed.add(evalPos)
                 incrementAround(posY,posX);
-              }
-              else if(input[posY][posX] == 0 && !flashed.has(evalPos)) {
-                input[posY][posX]++
+              } else if (!flashed.has(evalPos)){
+                if(posX == 0 & posY == 0) {
+                  console.log('figure out double increment')
+                }
+                // if(input[posY][posX] != 10 && input[posY][posX] != 0 && !flashed.has(evalPos)) {
+                  input[posY][posX]++
+                // }
+              } else {
+                console.log("Bad")
               }
             }
-          })
-
-          // Object.keys(positions).forEach(key => {
-          //   let val = positions[key]
-          //   let posX = val.position.x
-          //   let posY = val.position.y
-          //   let evalPos = {y: posY, x: posX}
-          //   // check if the position has flashed and if the value exists
-          //   if(val.flashCount != 99 && !flashed.has(JSON.stringify(evalPos))) {
-          //     if(val.flashCount >= 10) {
-          //       // increment flashes
-          //       console.log(`Current eval x:${x} y:${y}`)
-          //       console.log(`Flash at ${key} with pos x:${posX} y:${posY}`)
-          //       flashed.add(JSON.stringify(evalPos))
-          //       currentSum++;
-          //       input[posY][posX] = 0
-          //       currentSum += incrementAround(posY,posX)
-          //     } 
-          //     else {
-          //       input[posY][posX]++
-          //     }
-          //   }
-          // })
-      // } else if(!flashed.has(JSON.stringify(current))) {
-        // input[y][x]++
-      }
-    // }
-    // console.log(x,y, input)
+        })
+      // }
     return currentSum
 
 }
@@ -103,15 +90,20 @@ for(let n = 0; n < 2; n++) {
 
   for(let y=0; y < input[0].length; y++) {
     for(let x=0; x < input.length; x++) {
-      if(input[y][x] == 10) {incrementAround(y,x);}
-      totalFlash += flashed.size
+      if(input[y][x] >= 10) {
+        let temp = {y, x}
+        let evalPos = JSON.stringify(temp)
+        flashed.add(temp)
+        input[y][x] = 0
+        incrementAround(y,x);
+        totalFlash += flashed.size
+        flashed = new Set()
+      }
     }
   }
-  flashed = new Set()
   console.log(`step ${(n+1)} after flash`)
   console.log(input)
 
 }
-console.log("after flash", flashed)
 
 console.log("Total flashes", totalFlash)
